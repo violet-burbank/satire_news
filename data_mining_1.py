3e2a79347e5f8319823a79f432f1351db3ec4ea8
violet-burbank <burbank.violet@gmail.com>
2019-10-30 20:57:54 -0700

    pruning the data

--- data_mining_1.py
+++ data_mining_1.py
@@ -4,6 +4,14 @@ import pandas as pd
 csv_file_path = '/data/FakeNewsCorpus'
 os.chdir(os.path.join(os.getcwd(), "..", "..", ".."))
 os.chdir(os.path.join(os.getcwd(), "data", "FakeNewsCorpus"))
+print("trying now")
 data = pd.read_csv("news_cleaned_2018_02_13.csv")
 print(data.head())
 
+pruned_df = data['id', 'domain', 'type', 'url', 'title']
+
+print(pruned_df.head())
+
+pruned_df.to_csv('pruned_news.csv')
+
+print("done")
