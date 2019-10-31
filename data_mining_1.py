0b6c98294f9bf4100d5319085562c6fb0ce391fb
violet-burbank <burbank.violet@gmail.com>
2019-10-30 20:40:27 -0700

    accessing csv

--- data_mining_1.py
+++ data_mining_1.py
@@ -1,5 +1,9 @@
 import os
+import pandas as pd
 
 csv_file_path = '/data/FakeNewsCorpus'
-os.chdir(os.path.join(os.getcwd(), ".."))
-print(os.getcwd())
+os.chdir(os.path.join(os.getcwd(), "..", "..", ".."))
+os.chdir(os.path.join(os.getcwd(), "data", "FakeNewsCorpus"))
+data = pd.read_csv("news_cleaned_2018_02_13.csv")
+print(data.head())
+
