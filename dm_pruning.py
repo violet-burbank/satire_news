d0d9796e4d6005ba679606fe6188da0d298abec3
violet-burbank <burbank.violet@gmail.com>
2019-10-30 23:30:29 -0700

    fixed

--- dm_pruning.py
+++ dm_pruning.py
@@ -1,11 +1,12 @@
+# ssh vburbank@134.173.191.241 -p 5055
 import os
 import pandas as pd
 
 data = pd.read_csv("pruned_news.csv")
 
 print("pruning")
-pruned_df = pruned_df[pruned_df.'type' != 'unreliable']
-pruned_df = pruned_df[pruned_df.'type' != 'bias']  
+pruned_df = data[data.type != 'unreliable']
+pruned_df = pruned_df[pruned_df.type != 'bias']  
 
 domain_names = pruned_df[['domain']]
 
