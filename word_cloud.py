f8d0302f8821f0d9498eb877cc08470ee89edb4d
violet-burbank <burbank.violet@gmail.com>
2019-10-30 23:42:59 -0700

    word cloud

--- word_cloud.py
+++ word_cloud.py
@@ -5,16 +5,16 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("news_sample.csv")
+data = pd.read_csv("dom_names.csv")
 
 text = data[['domain']]
 wordcloud = WordCloud(
-    width = 3000,
-    height = 2000,
-    background_color = 'black',
+    width = 300,
+    height = 100,
+    background_color = 'white',
     stopwords = STOPWORDS).generate(str(text))
 fig = plt.figure(
-    figsize = (40, 30),
+    figsize = (10, 7),
     facecolor = 'k',
     edgecolor = 'k')
 plt.imshow(wordcloud, interpolation = 'bilinear')
